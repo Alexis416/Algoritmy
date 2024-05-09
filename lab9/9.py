637b62fe346896e2be4eadaac2a340e1b9d21ab7
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2024-05-10 00:11:45 +0300

    Create 9.py

---
+++ lab9/9.py
@@ -0,0 +1,37 @@
+import heapq
+
+
+def dijkstra(graph, start):
+    distances = {vertex: float('inf') for vertex in graph}
+    distances[start] = 0
+    queue = [(0, start)]
+
+    while queue:
+        current_distance, current_vertex = heapq.heappop(queue)
+
+        if current_distance > distances[current_vertex]:
+            continue
+
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+
+    return distances
+
+
+# Пример использования алгоритма
+graph1 = {
+    'A': {'B': 5, 'C': 2},
+    'B': {'A': 5, 'C': 1, 'D': 3},
+    'C': {'A': 2, 'B': 1, 'D': 1},
+    'D': {'B': 3, 'C': 1, 'E': 4},
+    'E': {'D': 4}
+}
+
+start_vertex = 'A'
+shortest_distances = dijkstra(graph1, start_vertex)
+
+print(shortest_distances)
